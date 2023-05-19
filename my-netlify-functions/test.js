14b65969db26b55e464de6f435098b5f684124b7
Fouls00 <ipax.fouls00@mail.ru>
2023-05-19 11:37:27 +0300

    Updated view of players' list in one message

--- my-netlify-functions/test.js
+++ my-netlify-functions/test.js
@@ -6,16 +6,19 @@ exports.handler = async function(event, context) {
             headers: { 'X-Auth-Token': 'af7450c5fc3541c3aede6334e63cb695' },
         });
         const data = response.data;
-        const players = data.squad.slice(0,10).map(player => ({
-            text: {
-                text: [`${player.position} - ${player.name}`]
-            }
-        }));
+        const players = data.squad.map(player => `${player.position} - ${player.name}`);
+        const allPlayers = players.join('\n'); // joins all player strings with a newline character
 
         return { 
             statusCode: 200, 
             body: JSON.stringify({
-                fulfillmentMessages: players
+                fulfillmentMessages: [
+                    {
+                        text: {
+                            text: [allPlayers]
+                        }
+                    }
+                ]
             }) 
         }
     } catch (error) {
