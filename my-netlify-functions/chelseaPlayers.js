8649d47b1487f60ed7e0b4abfad8f2a319e1e10d
Fouls00 <ipax.fouls00@mail.ru>
2023-05-19 14:44:58 +0300

    Fix issue v4 vs v2_2

--- my-netlify-functions/chelseaPlayers.js
+++ my-netlify-functions/chelseaPlayers.js
@@ -1,50 +1,48 @@
 const axios = require('axios');
 
-async function getPersonStats(personId) {
-  // Fetch person statistics from an endpoint
-  const response = await axios.get(`http://api.football-data.org/v4/persons/{id}`, {
-    headers: { 'X-Auth-Token': 'af7450c5fc3541c3aede6334e63cb695' },
-  });
-  const data = response.data;
-  
-  // Extract and return the necessary stats
-  const stats = {
-    matches: data.matches,
-    wins: data.wins,
-    // ...more stats
-  };
-  
-  return stats;
-}
+async function getPlayerNames() {
+    try {
+        const teamResponse = await axios.get('https://api.football-data.org/v4/teams/61', {
+            headers: { 'X-Auth-Token': 'af7450c5fc3541c3aede6334e63cb695' },
+        });
+        const teamData = teamResponse.data;
 
-exports.handler = async function(event, context) {
-  try {
-    const response = await axios.get('http://api.football-data.org/v4/teams/61', {
-        headers: { 'X-Auth-Token': 'af7450c5fc3541c3aede6334e63cb695' },
-    });
-    const data = response.data;
+        let players = [];
 
-    // An array to hold all our person stats promises
-    const personStatsPromises = [];
+        for (const player of teamData.squad) {
+            const playerResponse = await axios.get(`https://api.football-data.org/v4/persons/${player.id}`, {
+                headers: { 'X-Auth-Token': 'af7450c5fc3541c3aede6334e63cb695' },
+            });
+            const playerData = playerResponse.data;
 
-    for (const person of data.squad) {
-      // For each person, get their stats and store the promise in our array
-      personStatsPromises.push(getPersonStats(person.id));
+            let playerInfo = {
+                name: player.name,
+                position: player.position,
+                // Add or adjust these properties according to the actual structure of playerData
+                matches: playerData.matches,  
+                wins: playerData.wins,
+                cleanSheets: playerData.cleanSheets,
+                goals: playerData.goals,
+                assists: playerData.assists,
+                yellowCards: playerData.yellowCards,
+            };
+
+            players.push(playerInfo);
+        }
+
+        let messageText = players.map(player => `${player.position} - ${player.name}: Matches ${player.matches} / Wins ${player.wins} / Clean Sheets ${player.cleanSheets} / Goals ${player.goals} / Assists ${player.assists} / Yellow Cards ${player.yellowCards}`).join("\n");
+
+        return {
+            fulfillmentMessages: [{
+                text: {
+                    text: [messageText],
+                },
+            }],
+        };
+    } catch (error) {
+        console.log(error);
+        return { fulfillmentText: "Error occurred while fetching player information." };
     }
-
-    // Wait for all person stats promises to resolve
-    const allPersonStats = await Promise.all(personStatsPromises);
-
-    // Combine the person names, ids, and stats into the final output
-    const persons = data.squad.map((person, index) => `${person.position} - ${person.name} (${person.id}) - ${allPersonStats[index].matches} / ${allPersonStats[index].wins} / ...`);
-
-    const responseBody = {
-      fulfillmentText: persons.join('\n'),
-    };
-
-    return { statusCode: 200, body: JSON.stringify(responseBody) };
-  } catch (error) {
-    console.log(error);
-    return { statusCode: 500, body: JSON.stringify({ msg: error.message }) };
-  }
 };
+
+getPlayerNames();
