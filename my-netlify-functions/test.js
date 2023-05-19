026879487b16cab32abac5325c9dcfc4a8f468c3
Fouls00 <ipax.fouls00@mail.ru>
2023-05-19 11:26:31 +0300

    Updated view of players' list

--- my-netlify-functions/test.js
+++ my-netlify-functions/test.js
@@ -6,24 +6,21 @@ exports.handler = async function(event, context) {
             headers: { 'X-Auth-Token': 'af7450c5fc3541c3aede6334e63cb695' },
         });
         const data = response.data;
-        const players = data.squad.map(player => player.name);
-        const playersList = players.join(', ');
-
-        return {
-            statusCode: 200,
+        const players = data.squad.map(player => `${player.position} - ${player.name}`);
+        return { 
+            statusCode: 200, 
             body: JSON.stringify({
-                "fulfillmentText": "Here is the list of Chelsea players: " + playersList,
-                "fulfillmentMessages": [
+                fulfillmentMessages: [
                     {
-                        "text": {
-                            "text": [ playersList ]
+                        text: {
+                            text: players
                         }
                     }
                 ]
-            })
-        };
+            }) 
+        }
     } catch (error) {
         console.log(error);
-        return { statusCode: 500, body: JSON.stringify({ msg: error.message }) }
+        return { statusCode: 500, body: JSON.stringify({ msg: error.message }) };
     }
 };
