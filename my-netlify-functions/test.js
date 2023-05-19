9b0fb61d0c55840630a5f33eb1aeb37e9433629d
Fouls00 <ipax.fouls00@mail.ru>
2023-05-19 10:22:41 +0300

    First commit

---
+++ my-netlify-functions/test.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+
+exports.handler = async function(event, context) {
+    try {
+        const response = await axios.get('http://api.football-data.org/v4/teams/61', {
+            headers: { 'X-Auth-Token': 'af7450c5fc3541c3aede6334e63cb695' },
+        });
+        const data = response.data;
+        const players = data.squad.map(player => player.name);
+        return { statusCode: 200, body: JSON.stringify(players) };
+    } catch (error) {
+        console.log(error);
+        return { statusCode: 500, body: JSON.stringify({ msg: error.message }) };
+    }
+};
