815aca8708f42abfa9f124f830420dca8172a534
Fouls00 <ipax.fouls00@mail.ru>
2023-05-21 20:23:03 +0300

    try football-api

--- my-netlify-functions/getPlayers.js
+++ my-netlify-functions/getPlayers.js
@@ -1,47 +1,50 @@
 const axios = require('axios');
 
-async function getPlayerStats() {
-    try {
-        const teamData = await axios.get("https://api.football-data.org/v4/teams/61", {
-            headers: { 'X-Auth-Token': 'af7450c5fc3541c3aede6334e63cb695' }
-        });
-
-        let persons = teamData.data.squad;
-        let personsWithStats = [];
-
-        for (let person of persons) {
-            const personData = await axios.get(`https://api.football-data.org/v4/persons/${person.id}/matches`, {
-                headers: { 'X-Auth-Token': 'af7450c5fc3541c3aede6334e63cb695' }
-            });
-
-            let stats = personData.data.aggregations;
-            let personStats = {
-                name: person.name,
-                position: person.position,
-                matches: stats ? stats.matchesOnPitch : 'NA',
-                wins: stats ? stats.wins : 'NA',
-                goals: stats ? stats.goals : 'NA',
-                assists: stats ? stats.assists : 'NA',
-                yellowCards: stats ? stats.yellowCards : 'NA'
-            };
-
-            // Assign additional stats based on position
-            if (person.position === "Defender") {
-                personStats.dryMatches = stats ? stats.dryMatches : 'NA';
-            }
-
-            personsWithStats.push(personStats);
-        }
-
-        const responseBody = {
-            fulfillmentText: personsWithStats,
-        };
-
-        return { statusCode: 200, body: JSON.stringify(responseBody) };
-    } catch (error) {
-        console.log(error);
-        return { statusCode: 500, body: JSON.stringify({ msg: error.message }) };
-    }
+const apiKey = '<3eac65806bmshf36f1b84adebc7bp1533f2jsnd397ef53e279>';  // Replace <your-api-key> with the key obtained from RapidAPI
+const headers = {
+    'x-rapidapi-key': apiKey,
+    'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
+    'useQueryString': true
 }
 
-exports.handler = getPlayerStats;
+const team_id = 49;  // ID of Chelsea team on API-Football, double check this is still current
+
+async function getPlayers(team_id) {
+    const response = await axios.get(`https://api-football-v1.p.rapidapi.com/v3/players?team=${team_id}`, { headers });
+    return response.data.response;
+}
+
+async function getPlayerStats(player_id) {
+    const response = await axios.get(`https://api-football-v1.p.rapidapi.com/v3/players/${player_id}`, { headers });
+    return response.data.response[0].statistics;
+}
+
+exports.handler = async (event, context) => {
+    try {
+        const playersData = await getPlayers(team_id);
+        const playersWithStats = await Promise.all(playersData.map(async player => {
+            const statsData = await getPlayerStats(player.player.id);
+            const stats = statsData.find(stat => stat.team.id === team_id);
+            return {
+                name: player.player.name,
+                position: player.player.position,
+                matches: stats.games.appearences,
+                startingXI: stats.games.lineups,
+                goals: stats.goals.total,
+                assists: stats.goals.assists,
+                yellowCards: stats.cards.yellow,
+                redCards: stats.cards.red
+            };
+        }));
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(playersWithStats)
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ msg: error.message })
+        };
+    }
+}
